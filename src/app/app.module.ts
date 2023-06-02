0c5b60046e717429e8b1678fc0a086e800eca620
SSProject99 <135270950+SSProject99@users.noreply.github.com>
2023-06-02 10:50:24 +0530

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     ButtonComponent,
     ContentboxComponent,
-    FooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
